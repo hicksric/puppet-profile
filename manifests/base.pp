776fdbe147130a58fc8bb0c0de4ec4dd03d8ca32
root <root@richardhicks1c.mylabserver.com>
2019-02-25 13:30:22 +1300

    code updates

--- manifests/base.pp
+++ manifests/base.pp
@@ -2,7 +2,7 @@ class profile::base (
   $ntp_servers = [
     '0.ubuntu.pool.ntp.org',
     '1.ubuntu.pool.ntp.org',
-    '2.ubuntu.pool.ntp.org',
+    '3.ubuntu.pool.ntp.org',
   ],
 ) {
   include ::ssh
