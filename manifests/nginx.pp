70231e051c6ff6cc80c1757e8f64b3819af33e71
root <root@richardhicks1c.mylabserver.com>
2019-03-04 07:41:11 +1300

    nginx vhosts hieradata

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -1,14 +1,14 @@
 class profile::nginx(
-#  Array $vhosts,
+  Array $vhosts,
 ) {
   include ::nginx
 #  notify { "testdata":
 #    message => $vhosts;
 #  }
-#  $vhosts.each |$vhost| {
-    ::nginx::vhost { $facts['fqdn']:
-#      port            => $vhost['port'],
-#      server_aliases  => $vhost['server_aliases']
+  $vhosts.each |$vhost| {
+    ::nginx::vhost { $vhosts['fqdn']:
+      port            => $vhost['port'],
+      server_aliases  => $vhost['server_aliases']
     }
-#  }
+  }
 }
